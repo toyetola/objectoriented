f636d79b7dfb68faa7169d27868da72a48458bf2
toyetola <toyetolaabidemi@gmail.com>
2022-06-08 18:07:26 +0100

    Initial commit

---
+++ BankAccount.py
@@ -0,0 +1,45 @@
+
+class Account:
+
+    def __init__(self):
+        #self.accountNumber =
+        pass
+    _balance = 0.00
+
+    def getAccountNumber(self):
+        return 2136688776
+
+    def setter(self, amount, accountNumber):
+        validateAccount = self.getAccountNumber()
+        if amount > 0 and validateAccount == accountNumber:
+            self._balance = self._balance + amount
+        return self._balance
+
+    def getBalance(self):
+        return self._balance
+
+    def deposit(self, amount, accountNumber):
+        return self.setter(amount, accountNumber)
+
+    def withdraw(self, amount, accountNumber):
+        validateAccount = self.getAccountNumber()
+        if validateAccount != accountNumber:
+            return "Please provide the correct account"
+
+        balance = self._balance
+        if balance > amount:
+            self._balance = self._balance - amount
+            return "Take " + str(float(amount)) + " balance is " + str(self._balance)
+        return "Not enough balance"
+
+
+account = Account()
+print(account.getAccountNumber())
+print(account.getBalance())
+print(account.deposit(10000, 2136688776))
+print(account.deposit(12000, 2136688776))
+print(account.withdraw(12000, 2136688776))
+print(account.getBalance())
+
+
+
